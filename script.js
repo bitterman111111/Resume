4a485a87f3e756011f744e5c99aee129fa1dd8f6
bitterman111111 <139151714+bitterman111111@users.noreply.github.com>
2025-10-13 17:24:40 +0300

    try

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 
-function downloadPDF() {
-  const element = document.body;
-  html2pdf().from(element).save('page.pdf');
-}
+document.getElementById('downloadPdf').addEventListener('click', () => {
+    const element = document.body;
+
+    const opt = {
+        margin:       0,
+        filename:     'page.pdf',
+        image:        { type: 'jpeg', quality: 1.0 },
+        html2canvas:  { scale: 4, useCORS: true },
+        jsPDF:        { unit: 'pt', format: 'a4', orientation: 'portrait' }
+    };
+
+    html2pdf().set(opt).from(element).save();
+});
